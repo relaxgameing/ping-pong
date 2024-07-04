7c2dbbf78734db49b3b32aeeca3806d4e1284458
Ajay Gupta <ajaygupta@Ajays-MacBook-Pro.local>
2024-07-04 16:34:20 +0530

    added the timer between rounds

--- src/main.zig
+++ src/main.zig
@@ -7,11 +7,14 @@ const plank_SPEED: i32 = 5;
 
 var GAME_ENDED = false;
 var PAUSED = true;
+var ROUND_ENDED = true;
+
 var SCORE_Text = [_:0]u8{ '0', '0' };
 // [:0]const u8    *const [2:0]u8
 const Scores_array = [_]*const [2:0]u8{ "00", "01", "02", "03", "04", "05" };
 
-const winner_array = [_]*const [12:0]u8{ "player 1 won", "player 2 won" };
+const stringtemp = "player 1 won";
+const winner_array = [_]*const [12:0]u8{ stringtemp, "player 2 won" };
 var winner: u2 = undefined;
 
 const temparray = "00"; //*const [2:0]u8
@@ -26,6 +29,10 @@ var dx: i32 = 1;
 
 const screenWidth = 800;
 const screenHeight = 450;
+var delta: f32 = undefined;
+var gameTime: u32 = 0;
+var roundEndTime: u32 = 0;
+var pauseStartTime: u32 = 0;
 
 pub fn main() !void {
     std.debug.print("working", .{});
@@ -42,15 +49,18 @@ pub fn main() !void {
 
     // Main game loop
     while (!raylib.windowShouldClose()) { // Detect window close button or ESC key
+        // delta = raylib.getFrameTime() - @as(f32, raylib.getTime());
         // Update
-        const temp = raylib.getKeyPressed();
+        // const temp = raylib.getKeyPressed();
 
-        if (temp != raylib.KeyboardKey.key_null) {
-            std.debug.print("{any}\n", .{temp});
-            if (temp == raylib.KeyboardKey.key_space) {
-                PAUSED = true;
-            }
-        }
+        // if (temp != raylib.KeyboardKey.key_null) {
+        //     // std.debug.print("{any}\n", .{temp});
+        //     if (temp == raylib.KeyboardKey.key_space) {
+        //         PAUSED = true;
+        //         pauseStartTime = @intFromFloat(raylib.getTime());
+        //     }
+        // }
+        std.debug.print("{}\n", .{gameTime});
 
         scoreKeeper(ball);
 
@@ -60,15 +70,18 @@ pub fn main() !void {
             checkBallPlankCollision(&ball, &leftPlank);
             checkBallPlankCollision(&ball, &rightPlank);
 
-            ballBoundaryCheck(ball);
-
-            //update state
-            ball.y += @floatFromInt(ball_SPEED * dy);
-            ball.x += @floatFromInt(ball_SPEED * dx);
+            if (!ROUND_ENDED) {
+                updateBallState(&ball);
+            } else if (gameTime - roundEndTime == 3) {
+                ROUND_ENDED = false;
+            }
 
             plankBoundaryCheck(&leftPlank, .{ raylib.KeyboardKey.key_w, raylib.KeyboardKey.key_s });
             plankBoundaryCheck(&rightPlank, .{ raylib.KeyboardKey.key_up, raylib.KeyboardKey.key_down });
+            gameTime = @intFromFloat(raylib.getTime());
+            pauseStartTime = 0;
         }
+
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -80,7 +93,7 @@ pub fn main() !void {
 
         if (PAUSED) {
             displayMenu("menu");
-            displayText("PING PONG IN ZIG", 120);
+            displayText("PING PONG IN ZIG", 120, 20);
             const buttonState = displayButton("START BUTTON", 180);
             if (buttonState == 1) {
                 PAUSED = false;
@@ -89,8 +102,8 @@ pub fn main() !void {
             player1Score = 0;
             player2Score = 0;
             displayMenu("GAME ENDED");
-            displayText("GAME OVER", 120);
-            displayText(winner_array[winner], 180);
+            displayText("GAME OVER", 120, 20);
+            displayText(winner_array[winner], 180, 20);
             const buttonState = displayButton("RESTART", 220);
             if (buttonState == 1) {
                 GAME_ENDED = false;
@@ -98,29 +111,41 @@ pub fn main() !void {
             }
         } else {
             //game components
-            raylib.drawCircleV(ball, 5, raylib.Color.white);
+            if (ROUND_ENDED) {
+                displayText("next round in", screenHeight / 2 - 30, 22);
+                displayText(Scores_array[3 - (gameTime - roundEndTime)], screenHeight / 2 + 20, 20);
+            } else {
+                raylib.drawCircleV(ball, 5, raylib.Color.white);
+                raylib.drawRectangle(screenWidth / 2 - 2, 0, 4, screenHeight, raylib.Color.white);
+            }
             raylib.drawRectangleRec(leftPlank, raylib.Color.white);
             raylib.drawRectangleRec(rightPlank, raylib.Color.white);
 
             //center strip
-            raylib.drawRectangle(screenWidth / 2 - 2, 0, 4, screenHeight, raylib.Color.white);
 
             //players scores
-            raylib.drawText(Scores_array[player1Score][0..], (screenWidth / 4) - 10, 4, 16, raylib.Color.white);
-            raylib.drawText(Scores_array[player2Score][0..], screenWidth / 2 + (screenWidth / 4), 4, 16, raylib.Color.white);
+            raylib.drawText(Scores_array[player1Score][0..], (screenWidth / 4) - 10, 4, 20, raylib.Color.white);
+            raylib.drawText(Scores_array[player2Score][0..], screenWidth / 2 + (screenWidth / 4), 4, 20, raylib.Color.white);
         }
         //----------------------------------------------------------------------------------
     }
 }
 
-fn ballBoundaryCheck(ball: raylib.Vector2) void {
-    if (ball.y >= screenHeight or ball.y <= 0) {
+fn updateBallState(ball: *raylib.Vector2) void {
+    if (ball.*.y >= screenHeight or ball.*.y <= 0) {
         dy = -dy;
     }
 
-    if (ball.x >= screenWidth or ball.x <= 0) {
+    if (ball.*.x >= screenWidth + 40 or ball.*.x <= -40) {
         dx = -dx;
+        ball.*.y = screenHeight / 2 - 5;
+        ball.*.x = screenWidth / 2 - 5;
+        ROUND_ENDED = true;
+        roundEndTime = gameTime;
+        return;
     }
+    ball.*.y += @as(f32, @floatFromInt(ball_SPEED * dy));
+    ball.*.x += @as(f32, @floatFromInt(ball_SPEED * dx));
 }
 
 fn plankBoundaryCheck(plank: *raylib.Rectangle, key: [2]raylib.KeyboardKey) void {
@@ -134,11 +159,12 @@ fn plankBoundaryCheck(plank: *raylib.Rectangle, key: [2]raylib.KeyboardKey) void
 
 fn checkBallPlankCollision(ball: *raylib.Vector2, plank: *raylib.Rectangle) void {
     if (raylib.checkCollisionCircleRec(ball.*, 5, plank.*)) {
-        dx = -dx;
+        if (plank.*.x <= ball.*.x and ball.*.x <= plank.*.x + plank.*.width) {
+            dy = -dy;
+        } else {
+            dx = -dx;
+        }
     }
-    // if (raylib.checkCollisionCircleRec(ball, 5, leftPlank) or raylib.checkCollisionCircleRec(ball, 5, rightPlank)) {
-    //     dx = -dx;
-    // }
 }
 
 fn scoreKeeper(ball: raylib.Vector2) void {
@@ -158,9 +184,9 @@ fn displayMenu(title: [:0]const u8) void {
     _ = gui.guiGroupBox(raylib.Rectangle.init(200, 100, 400, 250), title);
 }
 
-fn displayText(text: [:0]const u8, y: i32) void {
+fn displayText(text: [:0]const u8, y: i32, fontSize: i32) void {
     const textWidth: i32 = @as(i32, raylib.measureText(text, 20));
-    raylib.drawText(text, 400 - @divExact(textWidth, 2), y, 20, raylib.Color.white);
+    raylib.drawText(text, 400 - @divExact(textWidth, 2), y, fontSize, raylib.Color.white);
 }
 
 fn displayButton(title: [:0]const u8, y: i32) i32 {
